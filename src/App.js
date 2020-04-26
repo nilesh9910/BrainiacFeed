31ddbc60fca0e7e13f5518b6cbff762e0eab5ea9
unknown <nileshkumar998@gmail.com>
2020-04-26 09:53:49 +0530

    Designed first nav bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import navbar from './Component/navbar';
+import Navbar from './Component/navbar';
 
 function App() {
   return (
     <div className="App">
-      <navbar />
+      <Navbar />
     </div>
   );
 }
--- src/Component/navbar.js
+++ src/Component/navbar.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react'
 
-class navbar extends Component {
+class Navbar extends Component {
     render() {
         return (
             <div>
-                <h1>Hello from navbar</h1>
+                <nav class="nav-extended">
+                    <div class="nav-wrapper">
+                    <a href="#" class="brand-logo">Logo</a>
+                    <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+                    <ul id="nav-mobile" class="right hide-on-med-and-down">
+                        <li><a href="sass.html">Sass</a></li>
+                        <li><a href="badges.html">Components</a></li>
+                        <li><a href="collapsible.html">JavaScript</a></li>
+                    </ul>
+                    </div>
+                </nav>
             </div>
         )
     }
 }
 
-export default navbar
+export default Navbar
