2aea6d59d29c5468860be607f8e8a5164ac590f5
unknown <nileshkumar998@gmail.com>
2020-04-26 09:29:05 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import navbar from './Component/navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <navbar />
+    </div>
+  );
+}
+
+export default App;
